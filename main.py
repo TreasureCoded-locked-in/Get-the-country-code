1324ef723d466286e47dc4f9c31898159d4847d1
TreasureCoded-locked-in <akpannsikak410@gmail.com>
2024-12-22 16:54:05 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+country_code = {'Nigeria' : '234',
+                'Australia' : '0025',
+                'Nepal' : '00977'}
+
+#search dictionary for country code of Nigeria
+print('Country code for Nigeria -')
+print(country_code.get('Nigeria','Not Found'))
+
+#search dictionary for country code of Japan
+print('Country code for Japan -')
+print(country_code.get('Japan','Not Found'))
